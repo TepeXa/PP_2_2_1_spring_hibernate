e93f85527582046c761709528dec60c45dbd2b81
Terentyev-DA <tepexa@gmail.com>
2022-06-21 17:58:49 +0500

    connect to DB

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -20,6 +22,11 @@ public class MainApp {
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
+      Car volvo = new Car("Volvo", 9);
+      Car bmw = new Car("BMW", 325);
+      Car suzuki = new Car("Sisuki", 4);
+      Car lada = new Car("Ladaa", 21014);
+
       List<User> users = userService.listUsers();
       for (User user : users) {
          System.out.println("Id = "+user.getId());
@@ -27,6 +34,26 @@ public class MainApp {
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
          System.out.println();
+      }
+
+     // userService.add(vasya.setCar(volvo).setUser(vasya));
+     // userService.add(petya.setCar(bmw).setUser(petya));
+     // userService.add(olga.setCar(suzuki).setUser(olga));
+     // userService.add(sveta.setCar(lada).setUser(sveta));
+
+      // пользователи с машинами
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
+      }
+
+      // достать юзера, владеющего машиной по ее модели и серии
+      System.out.println(userService.getUserByCar("BMW", 325));
+
+      // нет такого юзера с такой машиной
+      try {
+         User notFoundUser = userService.getUserByCar("GAZ", 4211);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
       }
 
       context.close();
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -5,6 +5,8 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
+   @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+   private Car car;
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -57,5 +59,24 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public Car setCar(Car car) {
+      this.car = car;
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "User {" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
    }
 }
